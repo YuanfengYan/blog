f57739373f5f4fee1a2661ffd8b27ebbd4b57ee3
YuanfengYan <yanyuanfeng_sspu@163.com>
2022-11-08 19:26:48 +0800

    更新

--- nuxt2-app/pages/blog.vue
+++ nuxt2-app/pages/blog.vue
@@ -1,67 +1,80 @@
 <template>
   <div class="pageWarp">
-    <el-row :gutter="10">
-      <el-col :xs="0" :sm="6">
-        <div class="category">
-          <el-divider><div class="categorytitle">分类</div></el-divider>
-          <el-radio-group
-            size="mini"
-            class="categoryWarp"
-            v-on:change="changeCategory"
-            v-model="categoryId"
-          >
-            <template v-if="Array.isArray(categoryList) && categoryList.length">
-              <el-radio
-                class="categoryItem"
-                v-for="item in categoryList"
-                :key="item.id"
-                :label="item.id"
-                border
-                >{{ item.name }}</el-radio
-              >
-            </template>
-          </el-radio-group>
-        </div>
-      </el-col>
-      <el-col :xs="24" :sm="18">
-        <ul
-          v-if="article && article.data && article.data.length > 0"
-          class="response-wrap article"
+    <div class="articleList">
+      <ul v-if="article && article.data && article.data.length > 0" class="article">
+        <li
+          v-for="item in article.data"
+          :key="item.id"
+          class="articleItem"
+          @click="jumpURL(item)"
         >
-          <li v-for="item in article.data" :key="item.id" class="article-list">
-            <a :href="'/article?id=' + item.id" class="article-item">
-              <div class="article-image">
-                <img :src="item.img_url" :alt="item.title" />
-              </div>
-
-              <div class="article-item-content">
-                <h1 class="article-title">
-                  {{ item.title }}
-                </h1>
-                <div class="article-description">
-                  {{ item.description }}
-                </div>
-
-                <div class="article-category">
-                  {{ item.category_info ? item.category_info.name : "" }}
-                </div>
-              </div>
-            </a>
-          </li>
-        </ul>
-        <div v-else class="empty-data">
-          暂无数据
-          <a v-if="isClear" href="/">清空搜索条件</a>
+          <div class="article-image">
+            <img :src="item.img_url" :alt="item.title" />
+          </div>
+          <div class="article-item-content">
+            <h1 class="article-title">
+              {{ item.title }}
+            </h1>
+            <div class="article-description">
+              {{ item.description }}
+            </div>
+            <div class="article-category">
+              {{ item.category_info ? item.category_info.name : "" }}
+            </div>
+          </div>
+        </li>
+      </ul>
+      <div v-else class="empty-data">
+        暂无数据
+        <a href="/blog">清空搜索条件</a>
+      </div>
+      <div v-if="isLoad" class="more" @click="loadMore">
+        <div class="more-text">点击加载更多</div>
+        <div class="more-arrow">
+          <img src="https://cdn.boblog.com/arrow.png" alt="" />
         </div>
-
-        <div v-if="isLoad" class="response-wrap more" @click="loadMore">
-          <div class="more-text">点击加载更多</div>
-          <div class="more-arrow">
-            <img src="https://cdn.boblog.com/arrow.png" alt="" />
+      </div>
+    </div>
+    <div class="info-wrapper">
+      <div class="personal-info-wrapper">
+        <h2>Yan's Blog</h2>
+        <div class="num">
+          <div>
+            <h3>5</h3>
+            <h6>Articles</h6>
+          </div>
+          <div>
+            <h3>10</h3>
+            <h6>Tags</h6>
           </div>
         </div>
-      </el-col>
-    </el-row>
+        <ul class="links">
+          <li>
+            <img src="@/assets/images/github.png" />
+          </li>
+        </ul>
+        <!-- <hr ></hr> -->
+        <el-divider content-position="left">分类</el-divider>
+        <el-radio-group
+          size="mini"
+          class="categoryWarp"
+          v-on:change="changeCategory"
+          v-model="categoryId"
+        >
+          <el-radio class="categoryItem" :key="all" border>全部</el-radio>
+          <template v-if="Array.isArray(categoryList) && categoryList.length">
+            <el-radio
+              class="categoryItem"
+              v-for="item in categoryList"
+              :key="item.id"
+              :label="item.id"
+              border
+              >{{ item.name }}</el-radio
+            >
+          </template>
+        </el-radio-group>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -140,6 +153,9 @@ export default {
       this.page++;
       this.fetchData();
     },
+    jumpURL(item) {
+      this.$router.push("/article?id=" + item.id);
+    },
   },
 };
 </script>
@@ -153,17 +169,66 @@ span.el-radio__input {
 </style>
 <style lang="scss" scoped>
 .pageWarp {
-  width: 1150px;
-  margin: 0 auto;
+  // width: 1150px;
+  display: flex;
+  align-items: flex-start;
+  margin: 20px auto 0;
+  padding: 0 20px;
+  max-width: 1100px;
 }
-.category {
-  margin-top: 40px;
-  width: 100%;
+
+.info-wrapper {
+  flex-basis: 400px;
+  margin-left: 15px;
+  transition: all 0.3s;
   overflow: hidden;
-  background-color: #eee;
-  .categorytitle {
-    font-size: 14px;
-    color: rgb(119, 5, 5);
+  top: 70px;
+  overflow: hidden;
+  height: auto;
+  box-shadow: 0 1px 8px 0 rgba(0, 0, 0, 0.1);
+  border-radius: 0.25rem;
+  box-sizing: border-box;
+  padding: 15px;
+  background: #fff;
+  .personal-info-wrapper {
+    & > h2 {
+      color: #000;
+      text-align: center;
+      margin: 10px auto 20px;
+    }
+  }
+  .num {
+    display: flex;
+    margin: 0 auto 10px;
+    width: 80%;
+    & > div:first-child {
+      border-right: 1px solid #333;
+    }
+    & > div {
+      flex-basis: 50%;
+      text-align: center;
+      h3 {
+        font-size: 16px;
+        line-height: 25px;
+      }
+      h6 {
+        line-height: 20px;
+        font-size: 12px;
+        color: #242424;
+      }
+    }
+  }
+  .links {
+    box-sizing: border-box;
+    display: flex;
+    flex-wrap: wrap;
+    padding: 10px;
+    img {
+      width: 22px;
+    }
+    hr {
+      border-top: 1px solid #eaecef;
+    }
   }
   .categoryWarp {
     width: 100%;
@@ -172,96 +237,96 @@ span.el-radio__input {
     justify-content: flex-start;
     align-items: center;
     flex-wrap: wrap;
-    padding: 0 15px 15px 20px;
     .categoryItem {
       display: inline-block;
       margin: 4px;
     }
   }
 }
-.article-list {
-  box-sizing: border-box;
-  display: block;
-  clear: both;
-  padding: 32px 0;
-  border-bottom: 1px solid #f0f0f0;
-}
-
-.article-list:hover .article-title {
-  color: #0164da;
-  text-decoration: underline;
-}
-
-.article-item {
-  display: flex;
-  height: 100%;
+.articleList {
   width: 100%;
-  text-decoration: none;
-  -webkit-transition: background-color 0.35s, color 0.35s, margin 0.45s,
-    -webkit-transform 0.5s;
-  transition: background-color 0.35s, color 0.35s, margin 0.45s, transform 0.5s;
-}
+  margin: 0 auto;
+  .article {
+    .articleItem {
+      box-sizing: border-box;
+      display: block;
+      margin: 0 auto 20px;
+      padding: 16px 20px;
+      border-bottom: 1px solid #f0f0f0;
+      text-decoration: none;
+      display: flex;
+      justify-content: flex-start;
+      align-items: center;
+      transition: all 0.2s linear;
+      box-shadow: 0 1px 8px 0 rgba(0, 0, 0, 0.1);
+      &:hover {
+        color: #0164da;
+        box-shadow: 0 2px 16px 0 rgba(0, 0, 0, 0.3);
+      }
+      .article-image {
+        width: 100px;
+        & img {
+          width: 100%;
+          border-radius: 4px;
+        }
+      }
 
-.article-image {
-  width: 100px;
-  & img {
-    width: 100%;
-    border-radius: 4px;
+      .article-item-content {
+        padding-left: 24px;
+        flex: 1;
+      }
+
+      .article-title {
+        font-weight: bold;
+        font-size: 18px;
+        color: #404040;
+        padding: 0;
+        margin: 0;
+      }
+
+      .article-description {
+        font-size: 14px;
+        color: #404040;
+        margin: 12px 0 24px;
+      }
+
+      .article-category {
+        font-size: 14px;
+        color: #808080;
+      }
+    }
   }
-}
-
-.article-item-content {
-  padding-left: 24px;
-  flex: 1;
-}
-
-.article-title {
-  font-weight: bold;
-  font-size: 18px;
-  color: #404040;
-  padding: 0;
-  margin: 0;
-}
-
-.article-description {
-  font-size: 14px;
-  color: #404040;
-  margin: 12px 0 24px;
-}
-
-.article-category {
-  font-size: 14px;
-  color: #808080;
-}
-
-.empty-data {
-  padding: 80px 0;
-  text-align: center;
-  font-size: 14px;
-}
-
-.more {
-  cursor: pointer;
-  padding: 32px 0;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  .more-text {
-    font-size: 16px;
-    font-weight: 400;
-    color: #222222;
-    line-height: 22px;
+  .empty-data {
+    padding: 80px 0;
+    text-align: center;
+    font-size: 14px;
   }
-  .more-arrow {
-    width: 16px;
-    margin-top: 24px;
-  }
-  .more-arrow img {
-    width: 100%;
+  .more {
+    cursor: pointer;
+    padding: 32px 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    .more-text {
+      font-size: 16px;
+      font-weight: 400;
+      color: #222222;
+      line-height: 22px;
+    }
+    .more-arrow {
+      width: 16px;
+      margin-top: 24px;
+    }
+    .more-arrow img {
+      width: 100%;
+    }
   }
 }
 
 @media screen and (max-width: 768px) {
+  .pageWarp {
+    display: block;
+  }
 }
 </style>
